5abccd34a3e3752ea8fd43617c9ba35b7996e48e
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2015-12-16 15:27:13 +0100

    cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,11 @@ class User < ActiveRecord::Base
     extend ActiveSupport::Concern
 
     included do
+      # associations
       has_many :user_roles, class_name: 'Ecm::UserArea::UserRole'
       has_many :roles, class_name: 'Ecm::UserArea::Role', through: :user_roles
 
       has_many :enabled_roles, -> { enabled }, class_name: 'Ecm::UserArea::Role', through: :user_roles, source: :user
-      # has_many :user_roles, class_name: 'Ecm::UserArea::UserRole'
-      # has_many :roles, class_name: 'Ecm::UserArea::Role', through: :user_roles
-      # has_many :role_permissions, class_name: 'Ecm::UserArea::Permission', through: :user_roles
-      # has_many :permissions, class_name: 'Ecm::UserArea::Permission', through: :role_permissions
-
-      # # has_many :enabled_roles, -> { enabled }, class_name: 'Ecm::UserArea::Role', through: :user_roles # , source: :user
-      # # has_many :enabled_permissions, -> { enabled }, class_name: 'Ecm::UserArea::Permission', through: :role_permissions # , source: :users
-      
-      # has_many :enabled_user_roles, class_name: 'Ecm::UserArea::UserRole'
-      # has_many :enabled_roles, -> { enabled }, class_name: 'Ecm::UserArea::Role', through: :enabled_user_roles, source: :role
-      # has_many :enabled_role_permissions, class_name: 'Ecm::UserArea::Permission', through: :enabled_user_roles
-      # has_many :enabled_permissions, -> { enabled }, class_name: 'Ecm::UserArea::Permission', through: :enabled_role_permissions # , source: :roles
     end
 
     def allowed_to?(permission_name)
