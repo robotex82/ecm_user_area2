e557b1be27068f295718eede03fdd8f5e46dccc2
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-02-04 16:58:08 +0100

    Better translations. Installer now adds routes and helpers.

--- lib/generators/ecm/user_area/install/install_generator.rb
+++ lib/generators/ecm/user_area/install/install_generator.rb
@@ -2,13 +2,29 @@ module Ecm
   module UserArea
     module Generators
       class InstallGenerator < Rails::Generators::Base
-        desc 'Generates the intializer'
+        desc 'Generates the initializer'
 
         source_root File.expand_path('../templates', __FILE__)
 
-        def generate_intializer
+        def generate_initializer
           copy_file 'initializer.rb', 'config/initializers/ecm_user_area.rb'
         end
+
+        def generate_routes
+          route File.read(File.join(File.expand_path('../templates', __FILE__), 'routes.source'))
+        end
+
+        def add_helpers_to_application_controller
+          inject_into_file 'app/controllers/application_controller.rb', before: "\nend" do
+            File.read(File.join(File.expand_path('../templates', __FILE__), 'application_controller.source'))
+          end
+        end
+
+        # def add_default_user
+        #   if yes? "Add default user (user@expample.com/password)?"
+        #     CreateDefaultUserService.call
+        #   end
+        # end
       end
     end
   end
