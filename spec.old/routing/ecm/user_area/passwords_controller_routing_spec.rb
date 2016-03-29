602fae462c9bf34c8f02f60a6c168ab485b0b074
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-03-29 22:46:18 +0200

    Cleanup.

--- spec.old/routing/ecm/user_area/passwords_controller_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-module Ecm::UserArea
-  describe PasswordsController do
-    it 'routes to #new' do
-      get('/en/users/password/new').should route_to('ecm/user_area/passwords#new', locale: 'en')
-    end
-  end
-end
--- spec.old/routing/ecm/user_area/sessions_controller_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-module Ecm::UserArea
-  describe SessionsController do
-    it 'routes to #sign_in' do
-      get('/en/users/sign_in').should route_to('ecm/user_area/sessions#new', locale: 'en')
-    end
-  end
-end
