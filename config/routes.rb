18b2f295e3bbc67673e80fc9e494f79ff8cdedd0
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-05-07 17:26:44 +0200

    Localize routes by default.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Ecm::UserArea::Engine.routes.draw do
-  resource :user, only: [:show, :edit, :update]
-  resource :user, only: [:new, :create] if Ecm::UserArea::Configuration.enable_registrations
-  resource :user, only: [:destroy] if Ecm::UserArea::Configuration.allow_users_to_destroy_self
-  resource :user_password_reset_request, only: [:new, :create]
+  localized do
+    scope :ecm_user_area do
+      resource :user, only: [:show, :edit, :update]
+      resource :user, only: [:new, :create] if Ecm::UserArea::Configuration.enable_registrations
+      resource :user, only: [:destroy] if Ecm::UserArea::Configuration.allow_users_to_destroy_self
+      resource :user_password_reset_request, only: [:new, :create]
 
-  resource :user_password_reset, only: [] do
-    get 'edit/:token' => 'user_password_resets#edit', constraint: { token: /\d+/ }, as: :edit
-    match '/:id/:token' => 'user_password_resets#update', constraint: { token: /\d+/ }, as: :update, via: [:put, :patch]
+      resource :user_password_reset, only: [] do
+        get 'edit/:token' => 'user_password_resets#edit', constraint: { token: /\d+/ }, as: :edit
+        match '/:id/:token' => 'user_password_resets#update', constraint: { token: /\d+/ }, as: :update, via: [:put, :patch]
+      end
+
+      resource :user_session, only: [:new, :create, :destroy]
+
+      root to: 'user#show'
+    end
   end
-
-  resource :user_session, only: [:new, :create, :destroy]
 end
