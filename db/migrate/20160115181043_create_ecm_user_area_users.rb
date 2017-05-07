c22b709b0d3b1e663cc546db7bac3807b77fda5e
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-05-07 17:26:36 +0200

    Add version.

--- db/migrate/20160115181043_create_ecm_user_area_users.rb
+++ db/migrate/20160115181043_create_ecm_user_area_users.rb
@@ -1,4 +1,4 @@
-class CreateEcmUserAreaUsers < ActiveRecord::Migration
+class CreateEcmUserAreaUsers < ActiveRecord::Migration[4.2]
   def change
     create_table :ecm_user_area_users do |t|
       # Authlogic::ActsAsAuthentic::Email
