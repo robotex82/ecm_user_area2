e0fbeca25990f9b5c108e8d9d27dfc87ca98a137
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-01-21 14:53:01 +0100

    Revert single_access_allowed_request_types to default setting.

--- app/sessions/ecm/user_area/user_session.rb
+++ app/sessions/ecm/user_area/user_session.rb
@@ -1,13 +1,6 @@
 module Ecm::UserArea
   class UserSession < Session::Base
-    # authenticate_with Ecm::UserArea::User
-
-    # attr_accessor :email, :password
-
-    # find_by_login_method :find_by_downcase_email
-
     params_key "#{self.name.demodulize.tableize.gsub('_sessions', '')}_api_key"
-    single_access_allowed_request_types = :json
     generalize_credentials_error_messages true
   end
 end
